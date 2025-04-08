b64aa1f9f3d425bf1abe2ffb26fe49050396f524
Dave Daniels <dennardavid1@gmail.com>
2025-04-08 23:15:06 +0100

    mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
         quality={100}
         priority={true}
         placeholder="blur"
-        className="absolute md:right-10 -bottom-[50%] md:bottom-15 -z-10"
+        className="absolute md:right-10 -bottom-[80%] md:bottom-15 -z-10"
       />
     </section>
   );
