ce34f6c1abf8fd0902d96bf5974db940475de5a1
Dave Daniels <dennardavid1@gmail.com>
2025-04-09 14:33:01 +0100

    Initial footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,67 @@
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa6";
+import { IoLogoFacebook } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+
 export default function Footer() {
   let year = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-lightBlack z-10 text-mainWhite">
-      {/* <div className="w-full max-width-[1500px]">
-        <p className=" text-sm">&#169; Luxury Rides {year} </p>
-      </div> */}
+    <footer className="text-xs md:text-sm w-full bg-lightBlack z-10 text-mainWhite p-5 flex flex-col items-center">
+      <div className="w-full max-w-[1500px]">
+        <section className="flex flex-row items-center justify-between w-full mb-3">
+          <div>
+            <Link href="#">Logo</Link>
+            <p className="mt-5">
+              Ride in Style <br />
+              Ride in Luxury
+            </p>
+          </div>
+          <div className="flex flex-row gap-5">
+            <Link
+              href="#"
+              className="text-gold hover:text-mainWhite !transition-all !duration-100"
+            >
+              <FaInstagram size={25} />
+            </Link>
+            <Link
+              href="#"
+              className="text-gold hover:text-mainWhite !transition-all !duration-100"
+            >
+              <IoLogoFacebook size={25} />
+            </Link>
+            <Link
+              href="#"
+              className="text-gold hover:text-mainWhite !transition-all !duration-100"
+            >
+              <FaXTwitter size={25} />
+            </Link>
+          </div>
+        </section>
+        <section className="flex justify-between mt-7">
+          <p className="text-center">&#169; {year} All Right Reserved </p>
+          <div className="flex flex-row justify-center mb-1.5">
+            <Link
+              href="#"
+              className=" hover:text-gold !transition-all !duration-100"
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              href="#"
+              className="before:content-['|'] before:mx-2 hover:text-gold !transition-all !duration-100"
+            >
+              Terms & Conditions
+            </Link>
+            <Link
+              href="#"
+              className="before:content-['|'] before:mx-2 hover:text-gold !transition-all !duration-100"
+            >
+              Cookies
+            </Link>
+          </div>
+        </section>
+      </div>
     </footer>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="flex justify-center !transition-all !duration-100">
       <nav className="z-20 flex fixed top-6 justify-between items-center w-[90vw] max-w-[1500px] backdrop-blur-2xl bg-lightBlack p-2 lg:p-3.5 rounded-full shadow-md border border-gold">
-        <Link href={"/"} className="text-base lg:text-2xl">
+        <Link href={"/"} className="text-base lg:text-2xl text-white">
           Logo
         </Link>
         <button className="flex items-center gap-2 lg:gap-3 text-base lg:text-xl bg-white p-1 lg:p-2 px-3.5 lg:px-5 rounded-full hover:cursor-pointer">
