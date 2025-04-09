65f6253e2963e15dace534e631ee0528b741869a
Dave Daniels <dennardavid1@gmail.com>
2025-04-09 12:44:30 +0100

    tablet view and mobile test

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,24 +3,24 @@ import backgroundImg from "../../public/prado.png";
 
 export default function Hero() {
   return (
-    <section className="mt-[25%] md:h-[90dvh] flex flex-col md:flex-row justify-between items-center max-w-[1500px] mx-auto relative px-4">
-      <div className="flex flex-col gap-3 md:gap-15 items-center">
-        <h1 className="text-lightBlack font-semibold text-4xl md:text-8xl text-center">
+    <section className="mt-[25%] md:mt-[14%] lg:mt-0 md:h-[90dvh] flex flex-col lg:flex-row items-center max-w-[1500px] mx-auto relative">
+      <div className="flex flex-col gap-3 lg:gap-15 items-center lg:items-start">
+        <h1 className="text-lightBlack font-semibold text-4xl md:text-7xl lg:text-8xl text-center lg:text-left">
           Ride in Style <br />
           Ride in Luxury
         </h1>
-        <p className="text-xs md:text-lg w-[90%] md:w-[45%] font-normal text-center">
+        <p className="text-xs md:text-sm lg:text-lg w-[90%] md:w-[60%] lg:w-[43%] font-normal text-center lg:text-left">
           Experience the thrill of riding in elegance. Whether it's a night out,
           a business trip, or a weekend escape, our fleet of luxury cars is
           ready to elevate your journey. Indulge in comfort, and prestige
           because you deserve nothing less.
         </p>
-        <button className="p-1 md:p-2 bg-lightBlack text-mainWhite rounded-full w-50 md:w-64 font-medium text-sm md:text-xl hover:cursor-pointer border-1 border-gold">
+        <button className="p-1 lg:p-2 bg-lightBlack text-mainWhite rounded-full w-50 lg:w-64 font-medium text-sm lg:text-xl hover:cursor-pointer border-1 border-gold">
           Ride Now
         </button>
       </div>
 
-      <div className="mt-4 md:mt-0 md:absolute md:right-10 md:bottom-15 md:-z-10">
+      <div className="mt-4 lg:mt-0 lg:absolute lg:-right-30 lg:bottom-3 lg:-z-10">
         <Image
           src={backgroundImg}
           alt="prado"
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,11 +4,11 @@ import { FiMenu } from "react-icons/fi";
 export default function Header() {
   return (
     <header className="flex justify-center !transition-all !duration-100">
-      <nav className="z-20 flex fixed top-6 justify-between items-center w-[90vw] md:w-full max-w-[1500px] backdrop-blur-2xl bg-lightBlack p-2 md:p-3.5 rounded-full shadow-md border border-gold">
-        <Link href={"/"} className="text-base md:text-2xl">
+      <nav className="z-20 flex fixed top-6 justify-between items-center w-[90vw] max-w-[1500px] backdrop-blur-2xl bg-lightBlack p-2 lg:p-3.5 rounded-full shadow-md border border-gold">
+        <Link href={"/"} className="text-base lg:text-2xl">
           Logo
         </Link>
-        <button className="flex items-center gap-2 md:gap-3 text-base md:text-xl bg-white p-1 md:p-2 px-3.5 md:px-5 rounded-full hover:cursor-pointer">
+        <button className="flex items-center gap-2 lg:gap-3 text-base lg:text-xl bg-white p-1 lg:p-2 px-3.5 lg:px-5 rounded-full hover:cursor-pointer">
           <FiMenu size={20} /> Menu
         </button>
       </nav>
